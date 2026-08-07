d3c8852d50175b50d856b2970abb7504158bdceb
JackRyanVJ <142451087+JackRyanVJ@users.noreply.github.com>
2026-08-08 00:48:44 +0530

    Add src/utils/supabase/server.ts

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,26 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kkyfwtezolkwfplzpyqt.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtreWZ3dGV6b2xrd2ZwbHpweXF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMDM0MjEsImV4cCI6MjEwMTY3OTQyMX0.eoisNt1SAYA6iqDyyy5h0KfqqhbQFBH1H2PoOBHV5ac';
+
+export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
+  return createServerClient(
+    supabaseUrl,
+    supabaseKey,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet: Array<{ name: string; value: string; options?: any }>) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => (cookieStore as any).set(name, value, options));
+          } catch {
+            // Ignored in Server Components
+          }
+        },
+      },
+    },
+  );
+};
