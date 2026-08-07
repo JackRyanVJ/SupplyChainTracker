63a195031b5e2eba13dba0c4f3e75d47bfc3fc47
JackRyanVJ <142451087+JackRyanVJ@users.noreply.github.com>
2026-08-08 00:48:43 +0530

    Add src/utils/supabase/middleware.ts

---
+++ src/utils/supabase/middleware.ts
@@ -0,0 +1,38 @@
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kkyfwtezolkwfplzpyqt.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtreWZ3dGV6b2xrd2ZwbHpweXF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMDM0MjEsImV4cCI6MjEwMTY3OTQyMX0.eoisNt1SAYA6iqDyyy5h0KfqqhbQFBH1H2PoOBHV5ac';
+
+export const updateSession = async (request: NextRequest) => {
+  let supabaseResponse = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    supabaseUrl,
+    supabaseKey,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet: Array<{ name: string; value: string; options?: any }>) {
+          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    },
+  );
+
+  await supabase.auth.getUser();
+
+  return supabaseResponse;
+};
